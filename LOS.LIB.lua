1290ed28bca2c27075230c313927b5159509e1be
Kyypie69 <markyyroblox@gmail.com>
2026-01-22 00:51:51 +0800

    Update LOS.LIB.lua

--- LOS.LIB.lua
+++ LOS.LIB.lua
@@ -153,7 +153,7 @@ function KyyfiiiLibrary:CreateWindow(Title, Description)
     Background.Parent = Main
     Background.BackgroundTransparency = 1
     Background.Size = UDim2.new(1,0,1,0)
-    Background.Image = "https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=116254090053371"
+    Background.Image = "https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=126406956523768"
     Background.ImageTransparency = .55
     Background.ImageColor3 = PALETTE.Accent
     cornerB.Parent = Background
